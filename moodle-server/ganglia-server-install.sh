699660ad2b1275341687ebd5c4c3201fb0a94d7e
Jeremy R Hajek <hajek@iit.edu>
2016-03-19 01:44:10 -0500

    Added rsyslog.conf template and updated installation script to copy rsyslog.conf in place

--- moodle-server/ganglia-server-install.sh
+++ moodle-server/ganglia-server-install.sh
@@ -4,7 +4,7 @@ set -v
 
 sudo add-apt-repository -y ppa:adiscon/v8-stable
 sudo apt-get update -y
-sudo apt-get install -y rsyslog
+sudo apt-get install -y rsyslog git 
 
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y ganglia-monitor rrdtool gmetad ganglia-webfrontend
 
@@ -12,3 +12,12 @@ sudo DEBIAN_FRONTEND=noninteractive apt-get install -y ganglia-monitor rrdtool g
 
 sudo cp /etc/ganglia-webfrontend/apache.conf /etc/apache2/sites-enabled/ganglia.conf
 
+git clone https://github.com/jhajek/ganglia-setup.git /tmp/ganglia
+
+sudo mv /tmp/ganglia/moodle-server/gmetad.conf /etc/ganglia/gmetad.conf
+
+sudo mv /tmp/ganglia/moodle-server/gmond.conf /etc/ganglia/gmond.conf
+
+sudo mv /tmp/ganglia/moodle-server/rsyslog.conf /etc/rsyslog.conf
+
+sudo service rsyslog restart
